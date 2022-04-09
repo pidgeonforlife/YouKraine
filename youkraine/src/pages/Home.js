b0dc526eb2b478f81041a051ef60ce4524f01292
AlanAyy <58347931+AlanAyy@users.noreply.github.com>
2022-04-09 06:28:38 -0400

    Update Home.js

--- youkraine/src/pages/Home.js
+++ youkraine/src/pages/Home.js
@@ -1,8 +1,25 @@
+import Nav from '../components/Nav'
+
 const Home = () => {
+
+    const authToken = true
+
+    const handleClick = () => {
+        console.log('Click!')
+    }
+
     return (
-        <div>
-            Home
+        <>
+        <Nav/>
+        <div className='home'>
+            <h1>Find Humanitarian Aid</h1>
+            <button className='primary-button'
+            onClick={handleClick}>
+                {authToken ? 'Sign Out' : 'Create Account'}
+            </button>
         </div>
+        </>
     )
 }
+
 export default Home
