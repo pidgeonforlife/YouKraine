3606166fb2dcb811d8063fa02c51bf480b3a5621
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-04-09 03:00:30 -0700

    Implemented the basic structure for the app

--- youkraine/src/App.js
+++ youkraine/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Onboarding from "./pages/Onboarding";
+import {  BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Dashboard' element={<Dashboard />}/>
+        <Route path='/Onboarding' element={<Onboarding />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ youkraine/src/pages/Onboarding.js
@@ -0,0 +1,8 @@
+const Onboarding = () => {
+    return (
+        <div>
+            Onboarding
+        </div>
+    )
+}
+export default Onboarding
