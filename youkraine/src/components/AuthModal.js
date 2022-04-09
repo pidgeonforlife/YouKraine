0e395e33381b01396632d93de699e12836c3cb35
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-04-09 03:36:02 -0700

    Pushing updated code

--- youkraine/src/components/AuthModal.js
+++ youkraine/src/components/AuthModal.js
@@ -1,7 +1,55 @@
+import { useState } from 'react'
 const AuthModal = () => {
+
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
+
+    const handleClick = () => {
+        setShowModal(false)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const isSignUp = true
+
     return (
-        <div>
-            Dashboard
+        <div className="auth-modal">
+            <div className="close-icon" onClick={handleClick}>X</div>
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+            <p>By clicking, suck it.</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password-check"
+                    id="password-check"
+                    name="password-check"
+                    placeholder="Confirm Password"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <input className='secondary-button' type="submit"/>
+                <p>{error}</p>
+
+            </form>
         </div>
     )
 }
